d7cf36abe955549ed1740fe9d74a5bfb2743a9b9
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2024-07-20 22:53:09 +0530

    ENV

--- sfeed_project/settings.py
+++ sfeed_project/settings.py
@@ -11,6 +11,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
+#DATABASES 
+
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'default')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'SudsAaFN4c8o')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'ep-shy-cell-a4b05nt6-pooler.us-east-1.aws.neon.tech')
+POSTGRES_DATABASE = os.getenv('POSTGRES_DATABASE', 'verceldb')
 
 #Development Envionment Settings
 '''
@@ -65,7 +71,7 @@ WSGI_APPLICATION = "sfeed_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 #Vercel
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -76,6 +82,22 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': POSTGRES_DATABASE,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASSWORD,
+        'HOST': POSTGRES_HOST,
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
+}
+
+
 '''
 #LOCAL
 
