b8ddb21ae2bf77e2549dc9c075dc93ab23b6cab4
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2024-07-20 22:46:14 +0530

    ADDED Comma

--- sfeed_project/settings.py
+++ sfeed_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 
 #Development Envionment Settings
